5155c52d536c8277486827766a9f0d6cd5156541
Victor Couto <101605675+victorCoutoDeRezende@users.noreply.github.com>
2024-02-07 17:00:18 -0300

    Add files via upload

---
+++ Pokedex 1/js/script.js
@@ -0,0 +1,76 @@
+const pokemonName = document.querySelector('.pokemon__name')
+const pokemonNumber = document.querySelector('.pokemon__number')
+const pokemonImage = document.querySelector('.pokemon__image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('.input__search')
+
+const buttonPrev = document.querySelector('.btn-prev')
+const buttonNext = document.querySelector('.btn-next')
+
+let searchPokemon = 1
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    if(APIResponse.status === 200){
+        const data = await APIResponse.json()
+        return data
+    }
+}
+
+const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...'
+    pokemonNumber.innerHTML = ''
+
+    const data = await fetchPokemon(pokemon)
+
+    if(data){
+        pokemonImage.style.display = 'block'
+        pokemonName.innerHTML = data.name
+        pokemonNumber.innerHTML = data.id
+        if(data.id <= 649){
+            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        }else if(data.id > 1010){
+            pokemonImage.src = data['sprites']['other']['official-artwork']['front_default']
+        }
+        else{
+            pokemonImage.src = data['sprites']['front_default']
+        }
+        
+        input.value = ''
+        searchPokemon = data.id
+    }else{
+        pokemonImage.style.display = 'none'
+        pokemonName.innerHTML = 'Not Found :c'
+        pokemonNumber.innerHTML = ''
+    }
+}
+
+form.addEventListener('submit', (ev)=>{
+    ev.preventDefault()
+    renderPokemon(input.value.toLowerCase())
+})
+
+buttonPrev.addEventListener('click', ()=>{
+    if(searchPokemon > 1){
+        searchPokemon -= 1
+        renderPokemon(searchPokemon)
+    }
+    else{
+        searchPokemon = 1017
+        renderPokemon(searchPokemon)
+    }
+})
+
+buttonNext.addEventListener('click', ()=>{
+    if(searchPokemon >= 1017){
+        searchPokemon = 1
+        renderPokemon(searchPokemon)
+    }else{
+        searchPokemon += 1
+        renderPokemon(searchPokemon)
+    }
+})
+
+
+renderPokemon(searchPokemon)
